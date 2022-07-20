f973bcf76c51d5c698b4b6610826bb0b140ff92a
CRgarcia15 <cg58708@gmial.com>
2022-07-19 20:38:29 -0400

    complete list of languages

---
+++ models/language.js
@@ -0,0 +1,15 @@
+//require mongoose
+const mongoose = require ('mongoose')
+const {Schema} = mongoose
+
+//schema
+const languageSchema = new Schema ({
+    name: {type: String, required: true},
+    greeting: String,
+    pangram: String,
+    filler: String
+})
+
+//exports
+const Language = mongoose.model('Language', languageSchema)
+module.exports = Language
